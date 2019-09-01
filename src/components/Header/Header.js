b6ed2cd40e48a55767696b0d8ed8f385c6b0bc16
FrozenPyrozen <kiril.kohan@gmail.com>
2019-09-01 11:01:04 -0400

    feat ( header )

---
+++ src/components/Header/Header.js
@@ -0,0 +1,5 @@
+import { connect } from 'react-redux';
+
+const Header = ({ lastMessageTime = Date.now().toLocaleString() }) => {};
+
+export default Header;
--- src/redux/chat/selectors/messagesSelector.js
+++ src/redux/chat/selectors/messagesSelector.js
@@ -1,9 +1,12 @@
 const getItems = state => state.chat.messages;
 const getLoading = state => state.chat.isLoading;
 const getError = state => state.chat.error;
+const getLastMessageTime = state =>
+  state.chat.messages[state.chat.messages.length - 1].created_at;
 
 export default {
   getItems,
   getLoading,
   getError,
+  getLastMessageTime,
 };
